cc20d28369d4aa81b78c6e6c5d1118153fc07887
nezuo <hinckleytmicah@gmail.com>
2022-10-13 18:19:52 -0700

    Initial commit

---
+++ src/export.rs
@@ -0,0 +1,172 @@
+use std::path::PathBuf;
+
+use image::{
+    imageops::{self, FilterType},
+    ImageBuffer, Rgba,
+};
+
+pub fn sequence_to_frames(image_paths: Vec<PathBuf>) -> Vec<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    let mut frames = Vec::new();
+
+    let frame_count = image_paths.len();
+
+    let frame_size = if frame_count > 16 {
+        128.0
+    } else if frame_count > 4 {
+        256.0
+    } else {
+        512.0
+    };
+
+    for image_path in image_paths {
+        let mut image = image::open(image_path).unwrap().into_rgba8();
+
+        let smaller = image.width().min(image.height()) as f64;
+        let higher = image.width().max(image.height()) as f64;
+
+        let (frame_width, frame_height) = if image.width() > image.height() {
+            (frame_size as u32, (frame_size * smaller / higher) as u32)
+        } else {
+            ((frame_size * smaller / higher) as u32, frame_size as u32)
+        };
+
+        image = imageops::resize(
+            &image,
+            frame_width as u32,
+            frame_height as u32,
+            FilterType::CatmullRom,
+        );
+
+        frames.push(image);
+    }
+
+    frames
+}
+
+pub fn image_to_frames(
+    rows: u32,
+    columns: u32,
+    image_path: PathBuf,
+) -> Vec<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+    let frame_count = rows * columns;
+
+    let frame_size = if frame_count > 16 {
+        128.0
+    } else if frame_count > 4 {
+        256.0
+    } else {
+        512.0
+    };
+
+    let mut image = image::open(image_path).unwrap().into_rgba8();
+
+    let old_frame_width = image.width() / rows;
+    let old_frame_height = image.height() / columns;
+
+    let smaller = old_frame_width.min(old_frame_height) as f64;
+    let higher = old_frame_width.max(old_frame_height) as f64;
+
+    let (frame_width, frame_height) = if old_frame_width > old_frame_height {
+        ((frame_size * smaller / higher) as u32, frame_size as u32)
+    } else {
+        (frame_size as u32, (frame_size * smaller / higher) as u32)
+    };
+
+    image = imageops::resize(
+        &image,
+        columns * frame_width as u32,
+        rows * frame_height as u32,
+        FilterType::CatmullRom,
+    );
+
+    let mut frames = Vec::new();
+
+    for frame in 0..frame_count {
+        let row = frame / columns;
+        let column = frame % columns;
+
+        let sub_image = imageops::crop_imm(
+            &image,
+            column * frame_width,
+            row * frame_height,
+            frame_width,
+            frame_height,
+        );
+
+        frames.push(sub_image.to_image());
+    }
+
+    frames
+}
+
+pub fn export_packed(frames: Vec<ImageBuffer<Rgba<u8>, Vec<u8>>>) {
+    let frame_count = frames.len();
+
+    let (rows_columns, frame_size): (u32, u32) = if frame_count > 16 {
+        (16, 128)
+    } else if frame_count > 4 {
+        (4, 256)
+    } else {
+        (2, 512)
+    };
+
+    let mut final_image = ImageBuffer::new(1024, 1024);
+
+    for (frame, image) in frames.iter().enumerate() {
+        let row = frame as u32 / rows_columns;
+        let column = frame as u32 % rows_columns;
+
+        let x = column * frame_size;
+        let y = row * frame_size;
+
+        imageops::overlay(&mut final_image, image, x.into(), y.into())
+    }
+
+    let path = rfd::FileDialog::new()
+        .add_filter("PNG", &["png"])
+        .save_file();
+
+    final_image.save(path.unwrap()).unwrap();
+}
+
+pub fn export_sequence(rows: u32, columns: u32, image_path: PathBuf) {
+    let temp = image_path.clone();
+    let file_name = temp
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .trim_matches('"');
+    let original_image = image::open(image_path).unwrap().into_rgba8();
+
+    let frame_width = original_image.width() / rows;
+    let frame_height = original_image.height() / columns;
+
+    let mut images = Vec::new();
+
+    for frame in 0..rows * columns {
+        let row = frame % columns;
+        let column = frame / rows;
+
+        let sub_image = imageops::crop_imm(
+            &original_image,
+            row * frame_width,
+            column * frame_height,
+            frame_width,
+            frame_height,
+        );
+
+        images.push(sub_image.to_image());
+    }
+
+    let path = rfd::FileDialog::new().pick_folder().unwrap();
+
+    for (i, image) in images.iter().enumerate() {
+        image
+            .save_with_format(
+                path.join(format!("{} ({}).png", file_name, i + 1)),
+                image::ImageFormat::Png,
+            )
+            .unwrap();
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,179 @@
+#![windows_subsystem = "windows"]
+
+mod export;
+
+use std::path::PathBuf;
+
+use eframe::{CreationContext, Frame, NativeOptions};
+use egui::{Button, CentralPanel, Context, TextureHandle, Ui, Vec2};
+
+fn main() {
+    eframe::run_native(
+        "Roblox Flipbook Packer",
+        NativeOptions::default(),
+        Box::new(|cc| Box::new(App::new(cc))),
+    )
+}
+
+struct App {
+    rows: u32,
+    columns: u32,
+    textures: Vec<TextureHandle>,
+    image_path: Option<PathBuf>,
+    sequence_path: Option<Vec<PathBuf>>,
+}
+
+impl App {
+    fn new(_: &CreationContext<'_>) -> Self {
+        App {
+            rows: 1,
+            columns: 1,
+            textures: Vec::new(),
+            image_path: None,
+            sequence_path: None,
+        }
+    }
+}
+
+fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
+}
+
+impl App {
+    fn render_import(&mut self, ui: &mut Ui) {
+        ui.group(|ui| {
+            ui.heading("Import");
+            ui.separator();
+
+            ui.horizontal(|ui| {
+                if ui.button("Import").clicked() {
+                    self.image_path = rfd::FileDialog::new().pick_file();
+                    self.sequence_path = None;
+
+                    let texture = ui.ctx().load_texture(
+                        "",
+                        load_image_from_path(self.image_path.as_ref().unwrap()).unwrap(),
+                        egui::TextureFilter::Linear,
+                    );
+
+                    self.textures = vec![texture];
+                }
+
+                if ui.button("Import Sequence").clicked() {
+                    self.sequence_path = rfd::FileDialog::new().pick_files();
+                    self.image_path = None;
+
+                    self.textures = Vec::new();
+
+                    let paths = self.sequence_path.as_ref().unwrap();
+
+                    for path in paths {
+                        let texture = ui.ctx().load_texture(
+                            "",
+                            load_image_from_path(path).unwrap(),
+                            egui::TextureFilter::Linear,
+                        );
+
+                        self.textures.push(texture);
+                    }
+                }
+            });
+
+            if !self.textures.is_empty() {
+                ui.horizontal(|ui| {
+                    for texture in &self.textures {
+                        let size = texture.size_vec2();
+
+                        let smaller = size.x.min(size.y) as f64;
+                        let higher = size.x.max(size.y) as f64;
+
+                        let size = if size.x > size.y {
+                            Vec2::new(128.0, (128.0 * smaller / higher) as f32)
+                        } else {
+                            Vec2::new((128.0 * smaller / higher) as f32, 128.0)
+                        };
+
+                        ui.image(texture, size);
+                    }
+                });
+            }
+
+            if self.image_path.is_some() {
+                ui.horizontal(|ui| {
+                    ui.label("Rows:");
+                    ui.add(
+                        egui::DragValue::new(&mut self.rows)
+                            .speed(0.1)
+                            .clamp_range(1..=256),
+                    );
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("Columns:");
+                    ui.add(
+                        egui::DragValue::new(&mut self.columns)
+                            .speed(0.1)
+                            .clamp_range(1..=256),
+                    );
+                });
+            }
+        });
+    }
+
+    fn render_export(&mut self, ui: &mut Ui) {
+        ui.group(|ui| {
+            ui.heading("Export");
+            ui.separator();
+
+            ui.horizontal(|ui| {
+                let pack_enabled = self.image_path.is_some() || self.sequence_path.is_some();
+
+                if ui.add_enabled(pack_enabled, Button::new("Pack")).clicked() {
+                    let sequence = match &self.image_path {
+                        Some(image_path) => export::image_to_frames(
+                            self.rows,
+                            self.columns,
+                            image_path.to_path_buf(),
+                        ),
+                        None => export::sequence_to_frames(
+                            self.sequence_path.as_ref().unwrap().to_vec(),
+                        ),
+                    };
+
+                    export::export_packed(sequence);
+                };
+
+                if ui
+                    .add_enabled(self.image_path.is_some(), Button::new("Export to Sequence"))
+                    .clicked()
+                {
+                    export::export_sequence(
+                        self.rows,
+                        self.columns,
+                        self.image_path.as_ref().unwrap().clone(),
+                    );
+                };
+            });
+        });
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, _: &mut Frame) {
+        ctx.set_visuals(egui::Visuals::dark());
+
+        CentralPanel::default().show(ctx, |ui| {
+            self.render_import(ui);
+            ui.add_space(6.0);
+            self.render_export(ui);
+        });
+    }
+}
